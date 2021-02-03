7abe877216445a8ef1514bc4cd34fc1515e5b142
Kacper Zielinski <kacper.zielinski@mailfence.com>
2021-02-03 21:39:30 +0100

    Disabled CORS support.

--- src/main/java/pl/rest/crud/config/DummyConfiguration.java
+++ src/main/java/pl/rest/crud/config/DummyConfiguration.java
@@ -1,8 +1,15 @@
 package pl.rest.crud.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class DummyConfiguration {
+public class DummyConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
 
 }
